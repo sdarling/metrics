8835239b0784d361570009787da83e5dd1a02354
Stephen Darling <stephen.darling@northramp.com>
2016-10-08 03:26:35 -0400

    metric level summaries

--- app/models/csf_function.rb
+++ app/models/csf_function.rb
@@ -1,4 +1,10 @@
 class CsfFunction < ActiveRecord::Base
 	has_many :metrics
 
+	def num_metrics
+
+		Metric.where("csf_function_id" => self.id).count(:id)
+
+	end
+
 end
--- app/models/metric.rb
+++ app/models/metric.rb
@@ -7,4 +7,18 @@ class Metric < ActiveRecord::Base
 		CsfFunction.find(self.csf_function_id).name
 	end
 
+	def first_entry
+		@old = MetricValue.where("metric_id" => self.id).order(:effective_date).first
+		@old.effective_date
+	end
+
+	def recent_entry
+		@recent = MetricValue.where("metric_id" => self.id).order(:effective_date).reverse_order.first
+		@recent.effective_date
+	end
+
+	def num_entries
+		MetricValue.where("metric_id" => self.id).count(:id)
+	end
+
 end
