35a1182419e0bf6ab3471eb4969fa8b81a3f0c06
Stephen Darling <stephen.darling@outlook.com>
2016-10-19 08:40:24 -0400

    workin in progress creating dashboard

--- app/controllers/csf_functions_controller.rb
+++ app/controllers/csf_functions_controller.rb
@@ -7,6 +7,11 @@ class CsfFunctionsController < ApplicationController
     @csf_functions = CsfFunction.all
     @score = CsfFunction.all.average(:avg_maturity_level)
     @recent_metrics = MetricValue.order(:effective_date).reverse_order.limit(5)
+    @identify = CsfFunction.where("id" => 1).first
+    @protect = CsfFunction.where("id" => 2).first
+    @detect = CsfFunction.where("id" => 3).first
+    @respond = CsfFunction.where("id" => 4).first
+    @recover = CsfFunction.where("id" => 5).first
   end
 
   # GET /metrics/1
