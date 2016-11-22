fc0608a8560bcf8cfa2c0827df0dc9539beca604
Stephen Darling <stephen_darling@abtassoc.com>
2016-11-22 08:59:20 -0500

    updated and working

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,2 +1,11 @@
 class Score < ActiveRecord::Base
+
+
+	def self.cron_update_score
+		score = CsfFunction.all.average(:avg_maturity_level)
+		target = Score.last.target_value
+		Score.create("effective_date" => DateTime.now, "actual_value" => score, "target_value" => target)
+	end
+
+
 end
